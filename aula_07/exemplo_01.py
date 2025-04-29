1c43c14d9ac2dcd7b5ba27200cd1a860912b5066
tecknicalle <joao.p.garcia.melo@gmail.com>
2025-04-29 20:48:17 -0300

    aula_07/exemplo_01.py
    git commit -m aula_07/exemplo_01.py

---
+++ aula_07/exemplo_01.py
@@ -0,0 +1,64 @@
+# Funções em python inicia com a palavra 
+# reservada "def".
+# Funções são rotinas em seu conceito
+# São blocos de códigos que só serão executados, se forem chamados.
+
+
+# def saudaçao():
+# print('olá mundo!')
+
+
+# saudaçao()
+# saudaçao()
+# saudaçao()
+
+
+# def mostrar_linha():
+    # print(30*"=")
+
+
+# mostrar_linha()
+# print("MÓDULO 01")
+# mostrar_linha()
+# print('ALGORITIMOS')
+# mostrar_linha()
+# print('ANÁLISE DE DADOS')
+# mostrar_linha()
+
+
+# def saudaçao(nome):
+    # print(f'olá ! {nome}!')
+
+
+# nome = input('informe o nome: ')
+# saudaçao(nome)
+
+
+# def somar(a, b):
+   # soma= a + b
+   # print(soma)
+   # return soma
+
+
+# soma = somar(4,5) 
+# print(f'O valor da variavel soma é {soma}')
+
+
+def somar_numeros(x,y):
+ s = x + y
+ return s
+
+
+for i in range(3)
+ n1 = int(input('informe o número:   '))
+ n2 = int(input('informe o número:   '))
+
+ soma = somar_numeros(n1, n2)
+ print(soma)
+ 
+
+
+
+
+ 
+
